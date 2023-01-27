5cc0e1ea17351eeeca642f9ad788eea2eaea619f
jefferson-Felipy <jeffersonthm1@gmail.com>
2023-01-27 17:47:02 -0300

    Change: Criação e atualização do diretorio do model de categoria

---
+++ models/Category.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Categoria = Schema({
+    name:{
+        type: String,
+        required:true
+    },
+    slug:{
+        type: String,
+        required:true
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+mongoose.model('categorias',Categoria);
+
+module.exports = Categoria;
